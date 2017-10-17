b3239f3c302c9dc1a5da6d199d52acaa09c9e988
ronnybt99 <32858991+ronnybt99@users.noreply.github.com>
2017-10-17 08:47:19 +0530

    Add files via upload

---
+++ JavaApplication1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+public class demo
+{
+public static void main(String [] args)
+{
+System.out.println("this is a trial");
+}
+}
